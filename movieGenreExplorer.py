fee120ce381502e3647aac388be26bf3758e6225
Matilde Ghidini <m.ghidini@campus.unimib.it>
2025-04-02 15:37:04 +0200

    Update movieGenreExplorer.py

--- movieGenreExplorer.py
+++ movieGenreExplorer.py
@@ -1,15 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Movie Genre Explorer")
+@st.cache
+def load_data():
+    df = pd.read_csv('cleaned_movies.csv')
+    return df
 
-df = pd.read_csv("cleaned_movies.csv")
+# Function to extract genres
+def get_unique_genres(df):
+    genres = df['genres'].str.split('|').explode().str.strip().unique()
+    return sorted(genres)
 
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
 
-st.dataframe(df, use_container_width=True)
+def main():
+    st.title("Movie Explorer by Genre")
 
-st.write("You selected:", option)
+    df = load_data()
+    
+    genres = get_unique_genres(df)
+
+    selected_genre = st.selectbox("Select Genre", genres)
+e
+    filtered_movies = df[df['genres'].str.contains(selected_genre, case=False, na=False)]
+
+    if not filtered_movies.empty:
+        st.write(f"Movies in the genre: {selected_genre}")
+        st.write(filtered_movies[['id', 'title', 'year', 'genres']])
+    else:
+        st.write(f"No movies found for the genre: {selected_genre}")
+
+if __name__ == "__main__":
+    main()
